f59fea799213abc7d2447c58940283706813690c
LillyYoshi <BrendanH1129@gmail.com>
2025-07-28 15:12:57 -0700

    debugging

--- LearnCPP/16.4.1/main.cpp
+++ LearnCPP/16.4.1/main.cpp
@@ -4,6 +4,7 @@
 template <typename T>
 void printElement(const std::vector<T>& arr, size_t indice) 
 {
+    std::cout << arr.size();
     if (!(indice > arr.size())) {
         std::cout << "The element has value: " << indice << '\n';
     }
