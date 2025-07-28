8e335370ddac1ba3dae105e9a999e7eadfee2004
LillyYoshi <BrendanH1129@gmail.com>
2025-07-28 15:11:20 -0700

    printElement of a array

---
+++ LearnCPP/16.4.1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void printElement(const std::vector<T>& arr, size_t indice) 
+{
+    if (!(indice > arr.size())) {
+        std::cout << "The element has value: " << indice << '\n';
+    }
+    else {
+        std::cout << "Invalid Index.";
+    }
+}
+    int main()
+{
+    std::vector<int> v1 { 0, 1, 2, 3, 4 };
+    printElement(v1, 2);
+    printElement(v1, 5);
+
+    std::vector<double> v2 { 1.1, 2.2, 3.3 };
+    printElement(v2, 0);
+    printElement(v2, -1);
+
+    return 0;
+}
